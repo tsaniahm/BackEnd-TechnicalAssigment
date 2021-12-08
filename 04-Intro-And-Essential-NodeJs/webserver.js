863fb8976ce1debff90c76093624b634cc93d01f
Tsaniah Munfidah <www.tsaniahmunfidah@gmail.com>
2021-12-08 20:01:54 +0700

    intro and essential node js done

---
+++ 04-Intro-And-Essential-NodeJs/webserver.js
@@ -0,0 +1,39 @@
+const http = require('http')
+
+const port = 3000
+
+const requestListener = (req, res) =>{
+    const urlEndpoint =  req.url
+
+    if(urlEndpoint === "/"){
+        const message = "Hello World!"
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(message)
+    }
+    else if(urlEndpoint === "/about"){
+        const message = "Welcome to about us page"
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(message)
+    }
+    else if(urlEndpoint === "/contact"){
+        const message = "Welcome to contact us page"
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(message)
+    }
+    else{
+        const message = "Hello World!"
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(message)
+    }
+
+    res.end()
+}
+
+const server = http.createServer(requestListener)
+
+server.on('listening', () => {
+    console.log(`server is listening on port http://127.0.0.1:${port}`)
+})
+
+server.listen(port)
+
